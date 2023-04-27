4805f03ad468f42ab8a667096e82ceabf91a6861
markb <markbuchholz8@gmail.com>
2023-04-27 13:14:30 -0600

    save

---
+++ src/views/TestingPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p> {{message}} </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+    export default {
+        data(){
+            return{
+                message: undefined,
+            }
+        },
+      mounted( ) {
+        axios.request({
+            url:`https:foodie.bymoen.codes/api/validate`,
+            headers: {
+            'x-api-key' : 'rnA2v1qeHqSIjeL98kXk'
+            },
+        }).then((response) => {
+            this.message = response[`data`]
+        }).catch((error) => {
+            error;
+        })
+      }  
+    }
+</script>
+
+<style scoped>
+
+</style>
