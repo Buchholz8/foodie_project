da0a086e4c716922e657ad0ba79ac53e04285ce8
markb <markbuchholz8@gmail.com>
2023-05-04 16:23:38 -0600

    fixes on login sign up

--- src/components/ClientSignup.vue
+++ src/components/ClientSignup.vue
@@ -1,42 +1,47 @@
 <template>
-    <div>
+  <div>
     <section>
-        <p>login</p>
-    <input type="email" placeholder="email" id="login_email">
-    <input type="password" placeholder="password" id="login_password">
-    <button @click="login_process">Log In</button>
+      <p>login</p>
+      <input type="email" placeholder="email" id="login_email" />
+      <input type="password" placeholder="password" id="login_password" />
+      <button @click="login_process">Log In</button>
     </section>
-    </div>
+  </div>
 </template>
 
 <script>
-import cookies from 'vue-cookies'
-import axios from 'axios'
-    export default {
-        login_process: function( ) {
-         axios.request({
-          url: "https://foodie.bymoen.codes/api/client-login",
-          headers: {
-            "x-api-key": "rnA2v1qeHqSIjeL98kXk",
-          },
-          method: "POST",
-          data: {
-            email: this.$ref[`login_email`].value,
-            password: this.$ref[`login_password`].value,
-          },
-        })
-        .then((response) => {
-          cookies.set(`token`, response[`data`][`token`]);
-          cookies.set(`client_id`  , response[`data`][`client_id`])
-          this.$router.push(`/views/ClientPFP.vue`)
-        })
-        .catch((error) => {
-          error;
-        });
-    }
-  }
+import cookies from "vue-cookies";
+import axios from "axios";
+export default {
+  signup_process: function () {
+    axios
+      .request({
+        url: "https://foodie.bymoen.codes/api/client",
+        headers: {
+          "x-api-key": "rnA2v1qeHqSIjeL98kXk",
+        },
+        method: "POST",
+        data: {
+          email: this.$ref[`email`].value,
+          password: this.$ref[`password`].value,
+          first_name: this.$ref[`first_name`].value,
+          last_name: this.$ref[`last_name`].value,
+          image_url: this.$ref[`profile_pic`].value,
+          username: this.$ref[`username`].value,
+        },
+      })
+      .then((response) => {
+        cookies.set(`token`, response[`data`][`token`]);
+        cookies.set(`client_id`, response[`data`][`client_id`]);
+        this.$router.push("/ClientPFP");
+      })
+      .catch((error) => {
+        error;
+        this.message = `something went wrong`;
+      });
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <section>
-        <h2>Please login or Sign up</h2>
-        <p>Sign up</p>
-    <input type="email" placeholder="email" ref="email" />
-    <input type="password" placeholder="password" ref="password" />
-    <input type="text" placeholder="first name" ref="first_name" />
-    <input type="text" placeholder="last name" ref="last_name" />
-    <input type="text" placeholder="username" ref="username" />
-    <input type="text" placeholder="image url" ref="profile_pic" />
-    <button @click="signup_process">Sign Up</button>
-    <p> {{message}} </p>
+      <h2>Please login or Sign up</h2>
+      <p>Sign up</p>
+      <input type="email" placeholder="email" ref="email" />
+      <input type="password" placeholder="password" ref="password" />
+      <input type="text" placeholder="first name" ref="first_name" />
+      <input type="text" placeholder="last name" ref="last_name" />
+      <input type="text" placeholder="username" ref="username" />
+      <input type="text" placeholder="image url" ref="profile_pic" />
+      <button @click="signup_process">Sign Up</button>
+      <p>{{ message }}</p>
     </section>
   </div>
 </template>
@@ -20,39 +20,36 @@ import axios from "axios";
 import cookies from "vue-cookies";
 
 export default {
-    data() {
-        return{
-            message: undefined
-        }
-    },
+  data() {
+    return {
+      message: undefined,
+    };
+  },
   methods: {
-    signup_process: function () {
-      axios.request({
-          url: "https://foodie.bymoen.codes/api/client",
+    login_process: function () {
+      axios
+        .request({
+          url: "https://foodie.bymoen.codes/api/client-login",
           headers: {
             "x-api-key": "rnA2v1qeHqSIjeL98kXk",
           },
           method: "POST",
           data: {
-            email: this.$ref[`email`].value,
-            password: this.$ref[`password`].value,
-            first_name: this.$ref[`first_name`].value,
-            last_name: this.$ref[`last_name`].value,
-            image_url: this.$ref[`profile_pic`].value,
-            username: this.$ref[`username`].value,
+            email: this.$ref[`login_email`].value,
+            password: this.$ref[`login_password`].value,
           },
         })
         .then((response) => {
           cookies.set(`token`, response[`data`][`token`]);
-          cookies.set(`client_id`  , response[`data`][`client_id`])
+          cookies.set(`client_id`, response[`data`][`client_id`]);
+          this.$router.push(`/views/ClientPFP.vue`);
         })
         .catch((error) => {
-            error;
-          this.message = `something went wrong`
+          error;
         });
     },
-}
-}
+  },
+};
 </script>
 
 <style scoped>
