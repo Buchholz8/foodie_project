28468894905fd4447785efd95fce6514ac650221
markb <markbuchholz8@gmail.com>
2023-05-06 09:26:49 -0600

    catching updated user info

--- src/components/ClientProfileInfo.vue
+++ src/components/ClientProfileInfo.vue
@@ -1,11 +1,39 @@
 <template>
-  <div></div>
+  <div>
+
+  </div>
 </template>
 
 <script>
-
+import axios from 'axios'
+import cookies from 'vue-cookies'
 export default {
-
+  mounted( ) {
+    this.get_client_info();
+    this.$root.$on(`client_info` , new_user_info => this.user_object = new_user_info)
+  },
+data() {
+  return {
+ user_object: undefined
+  }
+},
+methods: {
+get_client_info: function( ) {
+  axios.request({
+    url: 'https://foodie.bymoen.codes/api/client',
+    headers: {
+      'x-api-key': `rnA2v1qeHqSIjeL98kXk`
+    },
+    params: {
+      client_id : cookies.get(`client_id`)
+    }
+  }).then((response) => {
+    this.user_object = response[`data`][0]
+  }).catch((error) =>{
+    error
+  })
+}
+}
 };
 </script>
 
