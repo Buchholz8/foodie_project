98f4f0f992052d9c2f429bb3f91b684c27681e41
markb <markbuchholz8@gmail.com>
2023-05-09 17:40:43 -0600

    client order screen

--- src/components/order/ClientOrderInfo.vue
+++ src/components/order/ClientOrderInfo.vue
@@ -1,27 +1,43 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <p> {{this.message_result}} </p>
+  </div>
 </template>
 
 <script>
-import axios from 'axios' 
-import cookies from 'vue-cookies'
-    export default {
-        methods: {
-            client_order_handler: function( ) {
-                axios.request({ 
-                    url: 'https://foodie.bymoen.codes/api/client-order',
-                    headers: {
-                        "x-api-key": `rnA2v1qeHqSIjeL98kXk`,
-                    }
-
-                })
-            }
-        }
-    }
+import axios from "axios";
+import cookies from "vue-cookies";
+export default {
+  data() {
+    return {
+      order_val: undefined,
+      message_result: undefined
+    };
+  },
+  methods: {
+    client_order_handler: function () {
+      axios
+        .request({
+          url: "https://foodie.bymoen.codes/api/client-order",
+          headers: {
+            "x-api-key": `rnA2v1qeHqSIjeL98kXk`,
+            token: cookies.get("token"),
+          },
+          params: {
+            is_confirmed: true,
+            is_complete: true
+          }
+        })
+        .then((response) => {
+          this.order_val = response[`data`][0];
+        })
+        .catch((error) => {
+          this.message_result = error
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
